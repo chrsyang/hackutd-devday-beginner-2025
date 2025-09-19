1e676923739a55e37cfe0a6cbc690e60ed7c9e7c
chris yang <chrisyang@chriss-MacBook-Air.local>
2025-09-19 13:42:08 -0500

    update w changes

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
+    <div className="w-full bg-blue-100 py-15 px-8 border-b border-gray-300">
+      {/* Image */}
+      <img 
+        src="/download.jpeg" 
+        alt="background" 
+        className="w-full max-h-96 object-cover rounded-lg shadow-lg mb-6"
+      />
 
+      {/* Text */}
+      <div className="max-w-4xl mx-auto flex justify-center">
+        <div>
+          Maxing our GPA, One Class at a Time
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Hero
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import { User, Settings } from "lucide-react";
+import { User, Settings, GraduationCap } from "lucide-react";
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b border-gray-300">
+            <Link href= "/settings">
+                <Settings color="#F54927" className="cursor-pointer" />
+                <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0"/>
+            </Link>
+            <Link href= "/">
+                <GraduationCap color="#F54927" className="cursor-pointer"/>
+            </Link>
+            <Link href = "/">
+                <User color="#F54927" className="cursor-pointer"/>
+            </Link>
+        </nav>
     );
 }
 
